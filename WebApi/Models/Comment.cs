70a3ad34ec78d4b865295de35c11e0ed12df2bfc
vorvikqq <delodonn@ukr.net>
2025-08-16 13:42:09 +0300

    add Comment and Stock Models

---
+++ WebApi/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public int? StockId { get; set; }
+
+        // Navigation
+        public Stock? Stock { get; set; }
+    }
+}
