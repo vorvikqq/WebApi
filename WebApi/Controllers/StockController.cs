c4578636f1feb5b0af160374a0778734ce1b8216
vorvikqq <delodonn@ukr.net>
2025-08-16 14:38:32 +0300

    add stock controller

---
+++ WebApi/Controllers/StockController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/stock")]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList();
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+                return NotFound();
+
+            return Ok(stock);
+        }
+
+    }
+}
--- WebApi/Data/ApplicationDbContext.cs
+++ WebApi/Data/ApplicationDbContext.cs
@@ -6,8 +6,8 @@ namespace WebApi.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
-        public DbSet<Stock> Stock { get; set; }
-        public DbSet<Comment> Comment { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
     }
 }
