e1fb4263a6febe8eed27088de04a0aa9e8fc30b4
vorvikqq <delodonn@ukr.net>
2025-08-16 17:02:19 +0300

    add delete method

--- WebApi/Controllers/StockController.cs
+++ WebApi/Controllers/StockController.cs
@@ -63,5 +63,19 @@ namespace WebApi.Controllers
 
             return Ok(stockModel.ToStockDto());
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.Find(id);
+
+            if (stockModel == null)
+                return NotFound();
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
